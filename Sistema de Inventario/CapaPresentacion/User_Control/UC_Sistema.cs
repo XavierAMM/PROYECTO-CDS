8d94b89a76c943dc9a2814084de0bb623de17140
XavierAMM <xaviermancero790@gmail.com>
2023-08-16 14:10:21 -0500

    Update_1

--- Sistema de Inventario/CapaPresentacion/User_Control/UC_Sistema.cs
+++ Sistema de Inventario/CapaPresentacion/User_Control/UC_Sistema.cs
@@ -1,11 +1,39 @@
-﻿namespace CapaPresentacion.User_Control
+﻿using CapaPresentacion.User_Control.UC_Sistemas_Views;
+
+namespace CapaPresentacion.User_Control
 {
+	/// <summary>
+	/// En esta clase se encontrarán los elementos a cambiar del sistema en general. Módulos, opciones, perfiles y usuarios.
+	/// </summary>
 	public partial class UC_Sistema : UserControl
 	{
-		public UC_Sistema(int usuario_id)
+		int usuario_id;
+        /// <summary>
+        /// Constructor de la clase en la que se encontrarán los elementos a cambiar del sistema en general. Módulos, opciones, perfiles y usuarios.
+        /// </summary>
+        /// <param name="usuario_id">Id del usuario que ha ingresado al sistema.</param>
+        public UC_Sistema(int usuario_id)
 		{
 			InitializeComponent();
-
+			this.usuario_id = usuario_id;
 		}
+
+		/// <summary>
+		/// Muestra el user control específico en el panel de la derecha.
+		/// </summary>
+		/// <param name="uc">El User Control que se quiera mostrar.</param>
+		private void addUserControl(UserControl uc)
+		{
+			uc.Dock = DockStyle.Fill;
+			pnl_Main.Controls.Clear();
+			pnl_Main.Controls.Add(uc);
+			uc.BringToFront();
+		}
+
+		private void addViewModulos()
+		{
+			UC_Sistema_Modulo uc = new UC_Sistema_Modulo(usuario_id);
+		}
+
 	}
 }
